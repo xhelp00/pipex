5daa40c12b5f41349a65483835410f362950cd14
Petr Helebrant <xhelp00@gmail.com>
2023-04-25 15:00:29 +0200

    playing with processes

--- src/utils.c
+++ src/utils.c
@@ -6,7 +6,7 @@
 /*   By: phelebra <phelebra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 16:34:32 by phelebra          #+#    #+#             */
-/*   Updated: 2023/04/25 11:07:03 by phelebra         ###   ########.fr       */
+/*   Updated: 2023/04/25 14:28:28 by phelebra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,36 @@
 
 int	main(int argc, char *argv[])
 {
+	// fd[0] - read
+	// fd[1] - write
+	int fd[2];
+	if (pipe(fd) == -1)
+		printf("Error when opening pipe \n");
 	int id = fork();
-	int n;
-
 	if (id == 0)
 	{
-		n = 1;
+		close(fd[0]);
+		int x;
+		printf("Input number: ");
+		scanf("%d", &x);
+		if (write(fd[1], &x, sizeof(int)) == -1)
+		{
+			printf("error with writing to the pipe");
+			return 2;
+		}
+		close(fd[1]);
 	}
 	else
 	{
-		n = 6;
+		close(fd[1]);
+		int y;
+		if (read(fd[0], &y, sizeof(int)) == -1)
+		{
+			printf("error with read from the pipe");
+			return 3;
+		}
+		close(fd[0]);
+		printf("got from child process %d \n", y);
 	}
-	if (id != 0)
-		wait(0);
-	int i = n;
-	while (i < n + 5)
-	{
-		printf("%d", i);
-		fflush(stdout);
-		i++;
-	}
-	if (id != 0)
-		printf("\n");
-	
 	return (0);
 }
